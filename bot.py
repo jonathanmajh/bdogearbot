85210bf02f98a4b287fffe51d3ec16caf71333bd
Jonathan <jonathanmajh@gmail.com>
2020-09-12 19:58:47 -0400

    oops

--- bot.py
+++ bot.py
@@ -121,5 +121,6 @@ async def init_function(ctx):
     open(f'{HOME_PATH}gapp_cred.json', 'w+').write(GAPP_CRED)
     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = f'{HOME_PATH}gapp_cred.json'
     response = f'''```{os.listdir()}```'''
-    
+    await ctx.send(response)
+
 bot.run(TOKEN)
