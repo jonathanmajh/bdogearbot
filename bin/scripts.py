805d288e73c14ee96b4fb15831a553c35982445b
Jonathan <jonathanmajh@gmail.com>
2020-10-07 15:37:52 -0400

    add fallen god armor

--- bin/scripts.py
+++ bin/scripts.py
@@ -3,7 +3,7 @@ import os
 from metaphone import doublemetaphone
 
 # create html file by copying inner html of <tbody> under <table "MainItemTable_wrapper">
-with open('bdoitems2020-10-07.html', 'r') as reader:
+with open('bdoitems_import_example.html', 'r') as reader:
     line = reader.readline()
     counter = 1
     items = []
