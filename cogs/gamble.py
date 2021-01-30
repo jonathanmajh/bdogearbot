1459d068b9cd4d66be0db4e153b5b7eef183579c
Jonathan <jonathanmajh@gmail.com>
2021-01-30 18:23:22 -0500

    string formatting, chance to onetap

--- cogs/gamble.py
+++ cogs/gamble.py
@@ -8,49 +8,50 @@ import requests
 import math
 
 ACCESSORY_IDS = [
-    11607, # Ogre
-    11625, # Sicil
-    11628, # Serap
-    11629, # Tung Neck
-    11630, #Laytenn
-    11653, # Debore
-    11726, #Ruby
-    11727, #Sapphire
-    11728, # Topaz
-    11729, #Emerald
-    11730, #Diamond
-    11828, #Tung Ear
-    11834, #Narc
-    11853, #Disto
-    11855, #Dawn
-    11856, #Ethereal
-    11926, #Manos Ruby Earring
-    11927,# Manos Sapphire Earring
-    11928, #Manos Topaz Earring	
-    12031, #Crescent
-    12032, #Cadry
-    12042, #Ronaros
-    12060, #EyeRuin
-    12061, #TungRing
-    12068, #OminousRing
-    12126,#Manos Ruby Ring	
-    12128,#Manos Topaz Ring	
-    12229,#Centaurus Belt
-    12230,#Basilisk's Belt
-    12236,#Valtarra Eclipsed Belt
-    12237,#Tungrad Belt	
-    12251, #Orkinrad
-    12257, #turo
-    12326,#Manos Red Coral Belt
-    12327,#Manos Blue Coral Belt
-    12328,#Manos Green Coral Belt
-    12329,#Manos Golden Coral Belt	
-    12330,#Manos White Coral Belt
-    705509,#Manos Necklace
-    705510,#Manos Earring
-    705511,#Manos Ring
-    705512,#Manos Belt
+    11607,  # Ogre
+    11625,  # Sicil
+    11628,  # Serap
+    11629,  # Tung Neck
+    11630,  # Laytenn
+    11653,  # Debore
+    11726,  # Ruby
+    11727,  # Sapphire
+    11728,  # Topaz
+    11729,  # Emerald
+    11730,  # Diamond
+    11828,  # Tung Ear
+    11834,  # Narc
+    11853,  # Disto
+    11855,  # Dawn
+    11856,  # Ethereal
+    11926,  # Manos Ruby Earring
+    11927,  # Manos Sapphire Earring
+    11928,  # Manos Topaz Earring
+    12031,  # Crescent
+    12032,  # Cadry
+    12042,  # Ronaros
+    12060,  # EyeRuin
+    12061,  # TungRing
+    12068,  # OminousRing
+    12126,  # Manos Ruby Ring
+    12128,  # Manos Topaz Ring
+    12229,  # Centaurus Belt
+    12230,  # Basilisk's Belt
+    12236,  # Valtarra Eclipsed Belt
+    12237,  # Tungrad Belt
+    12251,  # Orkinrad
+    12257,  # turo
+    12326,  # Manos Red Coral Belt
+    12327,  # Manos Blue Coral Belt
+    12328,  # Manos Green Coral Belt
+    12329,  # Manos Golden Coral Belt
+    12330,  # Manos White Coral Belt
+    705509,  # Manos Necklace
+    705510,  # Manos Earring
+    705511,  # Manos Ring
+    705512,  # Manos Belt
 ]
+
 
 def refresh_data():
     time_now = datetime.now()
@@ -65,9 +66,11 @@ def refresh_data():
         prices.append(price)
     return prices
 
+
 def update_item_price(item):
     URL = f'https://marketweb-na.blackdesertonline.com/Trademarket/GetWorldMarketSubList'
-    r = requests.post(URL, headers={'Content-Type':'application/json', 'User-Agent':'BlackDesert'}, data="{'keyType' : 0, 'mainKey': "+str(item)+"}")
+    r = requests.post(URL, headers={'Content-Type': 'application/json',
+                                    'User-Agent': 'BlackDesert'}, data="{'keyType' : 0, 'mainKey': "+str(item)+"}")
     if r.status_code == 200:
         try:
             response = r.json()['resultMsg']
@@ -85,22 +88,36 @@ def update_item_price(item):
     else:
         return [False]
 
-def check_level(level):
-    LEVELS = {
-        'base': 1,
-        'pri': 2,
-        'duo': 3,
-        'tri': 4,
-        'tet': 5,
-        'pen': 6,
-    }
 
+LEVELS = {
+    'base': 1,
+    'pri': 2,
+    'duo': 3,
+    'tri': 4,
+    'tet': 5,
+    'pen': 6,
+}
+
+
+def check_level(level):
     if level.lower() in LEVELS:
         return LEVELS[level]
     else:
         return False
 
-millnames = ['',' K',' M',' B',' T']
+
+CHANCES = [1, 0.7, 0.5, 0.405, 0.3, 0.105]
+STACKS = [0, 18, 40, 44, 110, 200]
+
+def calc_probability(start, end):
+    chance = 1
+    for tap in CHANCES[start: end]:
+        chance = chance * tap
+    return chance
+
+
+millnames = ['', ' K', ' M', ' B', ' T']
+
 
 def millify(num):
     magnitude = 0
@@ -109,6 +126,7 @@ def millify(num):
         num /= 1000.0
     # add more suffixes if you need them
     return '%.2f%s' % (num, ['', 'K', 'M', 'G', 'T', 'P'][magnitude])
+
 
 class GambleCog(commands.Cog, name='EnchancementGambling'):
     def __init__(self, bot):
@@ -119,8 +137,8 @@ class GambleCog(commands.Cog, name='EnchancementGambling'):
         start = check_level(start)
         end = check_level(end)
         if start and end:
-            if calc=='%' or calc=='$':
-                await ctx.send('```Getting Market Prices for Accessories Please Wait```')
+            if calc == '%' or calc == '$':
+                await ctx.send('```Getting Market Prices for Accessories Please Wait\nChance to One Tap from {} to {}: {:.2%}```'.format(ctx.args[2].upper(), ctx.args[3].upper(), calc_probability(start, end)))
                 item_prices = refresh_data()
                 db = TinyDB(storage=MemoryStorage)
                 table = db.table(str(datetime.now()))
@@ -128,28 +146,30 @@ class GambleCog(commands.Cog, name='EnchancementGambling'):
                 for item in item_prices:
                     cost = item[start] + (item[1] * (end - start))
                     revenue = item[end] * 0.845
-                    if calc=='%':
+                    if calc == '%':
                         profit = revenue/cost
                     else:
                         profit = revenue-cost
-                    table.insert({'item_id':item[0], 'profit':profit})
+                    table.insert({'item_id': item[0], 'profit': profit})
                     item_ids.append(item[0])
                 sort = sorted(table.all(), key=lambda k: k['profit'])
                 sort.reverse()
                 sort[:20]
                 item_names = dict(get_item_by_id(item_ids))
-                msg = '```| Profit |    Name'
+                msg = '```|  Profit |    Name'
                 for thing in sort[0:20]:
-                    if calc=='%':
-                        msg = '{}\n| {:.0%}  | {}'.format(msg, thing['profit'], item_names[thing['item_id']])
+                    if calc == '%':
+                        profit_str = '{:.0%}'.format(thing['profit'])
+                        msg = f'{msg}\n|  {profit_str.rjust(6)} | {item_names[thing["item_id"]]}'
                     else:
-                        msg = '{}\n| $ {} | {}'.format(msg, millify(thing['profit']), item_names[thing['item_id']])
+                        msg = f'{msg}\n| ${millify(thing["profit"]).rjust(6)} | {item_names[thing["item_id"]]}'
                 msg = f'{msg}```'
                 await ctx.send(msg)
             else:
                 await ctx.send('Invalid profit type, please use "%" or "$"')
         else:
             await ctx.send('Invalid enhancement levels use: base, pri, duo, tri, tet, pen')
+
 
 def setup(bot):
     bot.add_cog(GambleCog(bot))
