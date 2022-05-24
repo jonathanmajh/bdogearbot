59ee7d6dfe8c4152d11b2bb43f26750d637913de
Jonathan Ma <jonathanmajh@gmail.com>
2022-05-24 15:14:15 -0400

    Update cloud_vision.py

--- bin/cloud_vision.py
+++ bin/cloud_vision.py
@@ -2,6 +2,13 @@ from fnmatch import fnmatch
 
 from bin.models import GearData, Result
 
+def same_line(text1, text2):
+    """check if text is somewhat on the same line"""
+    box1y = text1.bounding_poly.vertices[1].y + text1.bounding_poly.vertices[2].y
+    box2y = text2.bounding_poly.vertices[1].y + text2.bounding_poly.vertices[2].y
+    if abs(box1y - box2y) < 5:
+        return True
+    return False
 
 def detect_text(gear_data: GearData):
     """Detects text in the file."""
@@ -25,28 +32,28 @@ def detect_text(gear_data: GearData):
         print(text.description)
         if fnmatch(text.description, '*Attack*'):
             for j in range(count, len(texts)):
-                if texts[j].description.isnumeric():
+                if texts[j].description.isnumeric() & same_line(text, texts[j]):
                     gear_data.succ_ap = int(texts[j].description)
                     print(f'Found Succ AP: ${texts.pop(j)}')
                     scores_found = scores_found + 1
                     break
         if fnmatch(text.description, '*Awakening*'):
             for j in range(count, len(texts)):
-                if texts[j].description.isnumeric():
+                if texts[j].description.isnumeric() & same_line(text, texts[j]):
                     gear_data.awak_ap = int(texts[j].description)
                     print(f'Found Awak AP: ${texts.pop(j)}')
                     scores_found = scores_found + 1
                     break
         if fnmatch(text.description, '*Talent*'):
             for j in range(count, len(texts)):
-                if texts[j].description.isnumeric():
+                if texts[j].description.isnumeric() & same_line(text, texts[j]):
                     gear_data.awak_ap = int(texts[j].description)
                     print(f'Found Awak AP: ${texts.pop(j)}')
                     scores_found = scores_found + 1
                     break
         if fnmatch(text.description, '*Defense*'):
             for j in range(count, len(texts)):
-                if texts[j].description.isnumeric():
+                if texts[j].description.isnumeric() & same_line(text, texts[j]):
                     gear_data.dp = int(texts[j].description)
                     print(f'Found DP: ${texts.pop(j)}')
                     scores_found = scores_found + 1
